863e1a054bdd6ccede0cbd07e020a8b9cb9c6799
Remington Griffin <remington@utexas.edu>
2017-03-09 09:03:24 -0600

    Click

--- starter-code/click/base.js
+++ starter-code/click/base.js
@@ -2,4 +2,11 @@ console.log("Sanity Check: JS is working!");
 
 $(document).ready(function(){
 
-})
+  //var click = $('#imperatives').span();
+  $('#imperatives span').on('click', function(){
+    $('ul').append('<li>You clicked: '+$(this).text()+' at '+Date.now()+'</li>');
+
+  });
+//$('.parent').append('<p class="error">No Go</p>');
+
+});
