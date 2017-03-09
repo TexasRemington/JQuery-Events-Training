74d0e199a197c4db0092cb6510daae6ddec6f14e
Remington Griffin <remington@utexas.edu>
2017-03-08 19:39:23 -0600

    Change

--- starter-code/change/base.js
+++ starter-code/change/base.js
@@ -4,4 +4,11 @@ $(document).ready(function(){
 
   $("#time").text( Date.now() );
 
-})
+  $('#reset').on('click', function(){
+    $('#left').val('');
+    $('#right').val('');
+    $('#total').val('');
+  });
+  var random = 1 + Math.floor(Math.random() < 100);
+  $('.random').val(random);
+});
